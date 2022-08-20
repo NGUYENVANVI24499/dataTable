feb00cf21fadd6dde78e4c11ae05ce1ec16d108c
Nguyễn Văn Vĩ <43158136+NGUYENVANVI24499@users.noreply.github.com>
2022-08-20 22:08:52 +0700

    up code

--- src/data1.js
+++ src/data1.js
@@ -3,10 +3,7 @@ import React from 'react'
 const data1 = () => {
   return (
   <>
-    <div>datacuar main</div>
-    <div>thataj </div>
-    <div>abcgit </div>
-    <div>ddf</div>
+    
   </>
   )
 }
